288dd8bff2355ae8d8b8d5737183f80b5e346d75
onetayjones <taylordjone@gmail.com>
2023-03-18 18:51:29 +0200

    adding in progress bar

--- js/app.js
+++ js/app.js
@@ -1,22 +1,105 @@
 class CalorieTracker {
+    // this constructor renders each of these every time the app is loaded
     constructor() {
         // private constructor function
-        this._calorieLimit = 2000;
+        this._calorieLimit = 2200;
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
-    }
 
-    // public methods
+
+        this._displayCaloriesLimit();
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
+    }
+        
+        
+
+    // Public methods //
     addMeal(meal) {
         this._meals.push(meal);
-        this._totalCalories += meal.calories
+        this._totalCalories += meal.calories;
+        this._render();
     }
 
     addWorkout(workout) {
         this._workouts.push(workout);
-        this._totalCalories -= workout.calories
+        this._totalCalories -= workout.calories;
+        this._render();
     }
+
+    // Private Methods //
+
+    _displayCaloriesTotal(){
+        const totalCaloriesEl = document.getElementById('calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories;
+    }
+
+    _displayCaloriesLimit(){
+        const calorieLimitEl = document.getElementById('calories-limit');
+        calorieLimitEl.innerHTML = this._calorieLimit;
+    }
+
+    _displayCaloriesConsumed(){
+        const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+        const consumed = this._meals.reduce(
+            (total, meal) => total + meal.calories, 
+            0
+        );
+
+        caloriesConsumedEl.innerHTML = consumed;
+    }
+
+    _displayCaloriesBurned(){
+        const caloriesBurnedEl = document.getElementById('calories-burned');
+
+        const burned = this._workouts.reduce(
+            (total, workout) => total + workout.calories, 
+            0
+        );
+
+        caloriesBurnedEl.innerHTML = burned;
+    }
+
+    _displayCaloriesRemaining() {
+        const caloriesRemainingEl = document.getElementById('calories-remaining');
+        const progressEl = document.getElementById('calorie-progress')
+        const remaining = this._calorieLimit - this._totalCalories
+        caloriesRemainingEl.innerHTML = remaining;
+
+        if(remaining <= 0){
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+        } else {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-danger');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light');
+            progressEl.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+        }
+    }
+
+    _displayCaloriesProgress(){
+        const progressEl = document.getElementById('calorie-progress');
+        const percentage = (this._totalCalories / this._calorieLimit) * 100;
+        const width = Math.min(percentage, 100);
+        progressEl.style.width = `${width}%`;
+    }
+
+    // changes the calories when you add meal or workout, you call it in those methods
+    _render() {
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
+    }
+
 }
 
 class Meal {
@@ -38,3 +121,16 @@ class Workout {
     }
 }
 
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 1350);
+tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
+
+const run = new Workout('Morning Run', 320);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
