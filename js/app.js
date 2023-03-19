c6130840ba479652ac8fd2034768653983aa1aba
onetayjones <taylordjone@gmail.com>
2023-03-19 18:12:29 +0200

    clearing out local storage

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ class CalorieTracker {
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
         this._displayCaloriesProgress();
+
+        document.getElementById('limit').value = this._calorieLimit;
     }
         
         
@@ -67,6 +69,7 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        Storage.clearAll();
         this._render();
     }
 
@@ -305,6 +308,15 @@ class Storage {
         });
 
         localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static clearAll(){
+        localStorage.removeItem('totalCalories');
+        localStorage.removeItem('meals');
+        localStorage.removeItem('workouts');
+
+        // if you want to clear out everything
+        // localStorage.clear();
     }
 }
 
